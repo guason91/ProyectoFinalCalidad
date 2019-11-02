0f9066a1d3b1757558d2c44dc95d8a95707a3045
guason91 <solorzano.carlos261@gmail.com>
2019-11-01 18:16:52 -0600

    Prueba fallida

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "Carlos Solorzano";
+            return "prueba con error";
         }
 
         // POST api/values
